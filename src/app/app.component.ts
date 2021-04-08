31820b78d08189ca7ab6fdeb49dc5841e041832e
Pierre Paillard <paillard.pierre@gmail.com>
2021-04-08 16:46:28 +0200

    Ajout de for/if/simple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularSupportInputSimple';
+  // Valeur pour le input sans FOR
+  title:string = 'AngularSupportInputSimple';
+
+  // Valeur pour le input avec FOR
+  films: string[] = ["5eme element", "Alice au pays des merveilles", "Tenet"];
+
+  // l'utilisateur est admin ou pas?
+  isAdmin: boolean = true;
 }
